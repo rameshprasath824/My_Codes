481c6336743af7c936eb1a118118f4300fc3fff4
rameshprasath824 <rameshprasath824@gmail.com>
2021-10-26 02:37:03 +0530

    making code to choose random num on its own

--- python/practising python/guess_the_number.py
+++ python/practising python/guess_the_number.py
@@ -1,18 +1,26 @@
-number = 4
-range = 15
+import random
+
+number = random.randint(0,3)
+range = 3
 
 
 if range >= number:
-     guess = 0
-     chances = 1
-   while guess != number:
-     if 3 != chances and guess != number:
-        guess = int(input(f"Enter your first guess between 1 and {range}: "))
-        if guess < number:
-            print('Oops,you missed.number is too low')
-            chances+=1
-        if guess > number:
-            print('Oops,you missed. the number is too high')
-            chances+=1
-        if guess == number:
-            print('Hurreeey!! your guess is correct')
+ guess = 0
+ chances = 0
+ x = 0
+ while guess != number and chances != 3:
+     print(f'chances = {chances}')
+     guess = int(input(f"Enter your first guess between 1 and {range}: "))
+     if guess < number:
+       print('Oops,you missed.number is too low')
+       chances+=1
+     if guess > number:
+       print('Oops,you missed. the number is too high')
+       chances+=1
+     if guess == number:
+       print('Hurreeey!! your guess is correct')
+       chances+=1
+       x+=1
+
+ if x == 0:
+  print("you missed all three chances, sorry. better luck next time ")
