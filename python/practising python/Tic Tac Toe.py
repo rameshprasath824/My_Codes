1df81612167d9b02a536498a4ea159583397cf85
rameshprasath824 <rameshprasath824@gmail.com>
2021-11-02 22:01:01 +0530

    adding fucntions to mark square, to find available square and to check board is full or not

--- python/practising python/Tic Tac Toe.py
+++ python/practising python/Tic Tac Toe.py
@@ -1,11 +1,16 @@
 import pygame, sys
+import numpy as np
 
 # we should intialize pygame by using pygame.init()
 pygame.init()
 
+### CONSTANT VALUES ####
+
 # screen dimensions
 WIDTH = 600
 HEIGHT = 600
+BOARD_ROWS = 3
+BOARD_COLUMNS = 3
 
 # colors will be in (r , g , b) format
 RED = (255, 0, 0)
@@ -21,6 +26,10 @@ pygame.display.set_caption('Tic Tac Toe')
 
 screen.fill(BG_COLOR)
 
+# below np.zeros() function will create new array of given shape. here we kept 3 X 3
+board = np.zeros((BOARD_ROWS, BOARD_COLUMNS))
+print(board)
+
 # drawing the lines
 def drawing_lines():
    # drawing first horizontal line
@@ -31,6 +40,26 @@ def drawing_lines():
    pygame.draw.line(screen, LINE_COLOR, (200, 0), (200, 600), LINE_WIDTH)
    # drawing second vertical line
    pygame.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
+
+# this function will mark the square in the array
+def mark_square(row, col, player):
+    board[row][col] = player
+
+# this function checks square is available or not
+def availabe_square(row, col):
+    #return board[row][col] == 0 this line and below if and else statement will do same operation
+    if board[row][col] == 0:
+        return True
+    else:
+        return False
+
+# this function checks if the board is full or not
+def is_board_full():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLUMNS):
+            if board[row][col] == 0:
+                return False
+    return True
 
 drawing_lines()
 
